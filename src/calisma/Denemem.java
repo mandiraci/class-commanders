41c1fc81b54d7b3050a2c3de2259f99af1891c80
mustafamandiraci <mustafarya@yahoo.co.uk>
2024-08-07 21:17:50 -0400

    first push

---
+++ src/calisma/Denemem.java
@@ -0,0 +1,14 @@
+package calisma;
+
+public class Denemem {
+    public static void main(String[] args) {
+        System.out.println(" neee");
+
+        int num=5;
+        int num1=10;
+        System.out.println(num+num1);
+
+    }
+
+
+}
