dcd76b05d4921c3e6f8d01d9bb9bcf291c280290
mustafamandiraci <mustafarya@yahoo.co.uk>
2024-08-07 21:54:46 -0400

    persembe

--- src/calisma/Denemem.java
+++ src/calisma/Denemem.java
@@ -7,9 +7,10 @@ public class Denemem {
         int num=5;
         int num1=10;
         System.out.println(num+num1);
-
+        System.out.println();
     }
 
 
 }
-// de dersen o
+// de dersen o jhjhvjg
+//persembe gunu
